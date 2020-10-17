d15239e95a272d57d9e7d3bb8c0ee33b77406e37
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2020-10-17 17:23:25 +0200

    Update 1.2.2

--- module/cyphersystem.js
+++ module/cyphersystem.js
@@ -30,7 +30,7 @@ Hooks.once("init", async function() {
 	 * @type {String}
 	 */
 	CONFIG.Combat.initiative = {
-	  formula: "1d20 + @settings.initiative.initiativeBonus",
+	  formula: "d20 + @settings.initiative.initiativeBonus",
     decimals: 2
   };
 
