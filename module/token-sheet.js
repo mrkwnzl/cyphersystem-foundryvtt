070f2d4e4769b4d293c7c470650bf37fd629d7cc
NiceTSY <38940602+NiceTSY@users.noreply.github.com>
2021-03-19 11:25:15 +0100

    Revert " (optional] Wider sheets everywhere"
    This reverts commit a8d7be4194edc70b7d5ab9a0f4f00f89dcf7c1f6.

--- module/token-sheet.js
+++ module/token-sheet.js
@@ -9,8 +9,8 @@ export class CypherTokenSheet extends ActorSheet {
     return mergeObject(super.defaultOptions, {
       classes: ["cyphersystem", "sheet", "actor", "token"],
       template: "systems/cyphersystem/templates/token-sheet.html",
-      width: 640,
-      height: 595,
+      width: 600,
+      height: 560,
       resizable: false,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body"}],
       scrollY: [".sheet-body", ".tab", ".skills", ".description", ".items", ".settings"],
