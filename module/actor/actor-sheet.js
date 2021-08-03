2a772b81eee0ef08f239fd9f2e58bd9be97b5f2b
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2021-08-03 18:33:26 +0200

    Hotfix for auto-roll buttons

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -76,7 +76,7 @@ export class CypherActorSheet extends ActorSheet {
       if (actorData.data.settings.hideArchived && i.data.archived) hidden = true;
 
       // Check for roll button on level
-      if (Roll.validate(i.data.level) && i.data.level && isNaN(i.data.level)) {
+      if (Roll.validate(i.data.level.toString()) && i.data.level && isNaN(i.data.level)) {
         i.data.rollForLevel = true;
       } else {
         i.data.rollForLevel = false;
