0e09f81980f6c99faef9f630e3f7cec568ca9708
farling42 <github@amusingtime.uk>
2023-05-14 10:47:31 +0100

    Revert "First jab at getting pools updated properly"
    This reverts commit abeeca9813d9fe0252b1f991c7dbc39b311da49e.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -4,32 +4,4 @@
  */
 export class CypherActor extends Actor {
   static LOG_V10_COMPATIBILITY_WARNINGS = true;
-
-  _updatePoolValue(beforepool, before, after, stat, changes) {
-    let pre  = before[`system.pools.${stat}.max`] ?? beforepool[`${stat}.max`];
-    let post = after[`system.pools.${stat}.max`] ?? this.system.pools[stat].max;
-    if (pre != post) {
-      let diff = post - pre;
-      if (diff != 0) {
-        if (changes===undefined) changes={};
-        setProperty(changes, `system.pools.${stat}.value`, this.system.pools[stat].value + diff);
-      }
-    }
-    return changes;
-  }
-
-  // If max pools have changed, then change corresponding value
-  applyActiveEffects() {
-    let before_pools    = foundry.utils.flattenObject(this.system.pools);
-    let before_override = foundry.utils.flattenObject(this.overrides);
-    super.applyActiveEffects();
-    let after_pools = foundry.utils.flattenObject(this.overrides);
-    console.log('applyActiveEffects', before_pools, after_pools);
-    let changes;
-    changes = this._updatePoolValue(before_pools, before_override, after_pools, "might", changes);
-    changes = this._updatePoolValue(before_pools, before_override, after_pools, "speed", changes);
-    changes = this._updatePoolValue(before_pools, before_override, after_pools, "intellect", changes);
-    changes = this._updatePoolValue(before_pools, before_override, after_pools, "additional", changes);
-    //if (changes) this.update(changes);
-  }
 }
